6f4e81060a0b58f23eca995533eb12b71643d81a
Devil64-Dev <devil64dev@gmail.com>
2020-05-27 16:39:29 -0500

    This class handle clicks on RecyclerView list_options

--- app/src/main/java/com/devil64dev/myfilerename/activities/ReplaceActivity.java
+++ app/src/main/java/com/devil64dev/myfilerename/activities/ReplaceActivity.java
@@ -1,4 +1,22 @@
 package com.devil64dev.myfilerename.activities;
 
-public class ReplaceActivity {
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.devil64dev.myfilerename.R;
+
+public class ReplaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replace);
+        ActionBar actionBar = getSupportActionBar();
+        Resources res = getResources();
+        String title = res.getStringArray(R.array.options_title)[0];
+        actionBar.setTitle(title);
+    }
 }
