ce44bf54b5cc0d16ac0519aedca0bbaca9fa25ec
Devil64-Dev <devil64dev@gmail.com>
2020-05-27 09:00:05 -0500

    Add RecylcleView view adapter

--- app/src/main/java/com/devil64dev/myfilerename/MyAdapter.java
+++ app/src/main/java/com/devil64dev/myfilerename/MyAdapter.java
@@ -1,4 +1,62 @@
 package com.devil64dev.myfilerename;
 
-public class MyAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] dataTitles;
+    private String[] dataDescription;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        View view;
+        MyViewHolder(View v) {
+            super(v);
+            view = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    MyAdapter(String[] myDataTitles, String[] myDataDescription) {
+        dataTitles = myDataTitles;
+        dataDescription = myDataDescription;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_card, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        TextView title = holder.view.findViewById(R.id.card_title);
+        title.setText(dataTitles[position]);
+
+        TextView description = holder.view.findViewById(R.id.card_description);
+        description.setText(dataDescription[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataTitles.length;
+    }
 }
+    
